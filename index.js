d96a0b54979f0849ace86673cc1bfe64a6de6524
Boniface <bm715452@gmail.com>
2025-03-20 02:35:45 +0300

    teh dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute of the newHeader to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the newHeader to "YOUR-NAME is the champion"
+newHeader.textContent = "Boniface Mwangi is the champion"; //Reaplce YOUR-NAME with your actual name
+
+// Append teh newHeader to the body (or any other desired location)
+document.body.appendChidl(newHeader);
